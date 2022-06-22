e5dc1382ce0198f596f18c14e87be12d4e62ae18
abhijayrajvansh <abhijayrajvansh01@gmail.com>
2022-06-22 11:59:15 +0530

    Added Installation Commands and Modules ✅

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Installing Package Manager "HOMEBREW" 🍺
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Running this for all the important updates ⚙️
+brew update --auto-update
+
+# Installing Fish 🐟
+brew install fish
